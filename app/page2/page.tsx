4ade68eabbfcacb575edf670182d7caf53cdb05b
Helba <helbashoby2007@gmail.com>
2026-06-05 18:05:38 +0530

    Add nav and card components to page2

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -1,9 +1,13 @@
+import Nav from "./nav";
 import Hero from "./hero";
+import Card from "./card";
 
 export default function Page() {
   return (
     <main>
+      <Nav />
       <Hero />
+      <Card />
     </main>
   );
 }
