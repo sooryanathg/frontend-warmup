6288054799f24141fa4329c5f0d4158490df83a7
Megha <meghamg11@gmail.com>
2026-06-06 19:15:50 +0530

    Add navbar component

--- components/page2/nav.tsx
+++ components/page2/nav.tsx
@@ -1,7 +1,43 @@
 export default function Nav() {
   return (
-    <nav>
-      Nav
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "16px 40px",
+        background: "#f9fafb",
+        borderBottom: "1px solid #ddd",
+        fontFamily: "Arial",
+      }}
+    >
+      <h2
+        style={{
+          color: "blue",
+          margin: 0,
+        }}
+      >
+        IT Department 🚀
+      </h2>
+
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+        }}
+      >
+        <a href="#" style={{ textDecoration: "none", color: "#333" }}>
+          Home
+        </a>
+
+        <a href="#" style={{ textDecoration: "none", color: "#333" }}>
+          About
+        </a>
+
+        <a href="#" style={{ textDecoration: "none", color: "#333" }}>
+          Contact
+        </a>
+      </div>
     </nav>
   );
 }
