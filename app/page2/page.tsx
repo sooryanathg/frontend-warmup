364bebff6ffb0039025e1ad8feda70844be684de
Helba <helbashoby2007@gmail.com>
2026-06-06 18:13:58 +0530

    Move page2 components to components folder

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -1,6 +1,6 @@
-import Nav from "./nav";
-import Hero from "./hero";
-import Card from "./card";
+import Nav from "../../components/ui/page2/nav";
+import Hero from "../../components/ui/page2/hero";
+import Card from "../../components/ui/page2/card";
 
 export default function Page() {
   return (
